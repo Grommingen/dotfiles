3c0b23c9083e7294a06d8ec91df33963d4744814
Bjørn-Ivar <bjohoi@redpill-linpro.com>
2025-05-16 23:37:32 +0200

    chore: fix fish config for homebrew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,17 +11,17 @@ direnv hook fish | source
 set -g direnv_fish_mode eval_on_arrow
 set -gx EDITOR vim
 # source functions
-source /home/bjohoi/.config/fish/functions/__fish_complete_directories_homemade_tf.fish
-source /home/bjohoi/.config/fish/functions/__fish_complete_directories_homemade_puppet.fish
-source /home/bjohoi/.config/fish/functions/__fish_complete_directories_homemade_workpass.fish
-source /home/bjohoi/.config/fish/functions/tfjump.fish
-source /home/bjohoi/.config/fish/functions/puppetjump.fish
-# source completions
-source /home/bjohoi/.config/fish/completions/tfj_complete.fish
-source /home/bjohoi/.config/fish/completions/ppj_complete.fish
-source /home/bjohoi/.config/fish/completions/workpasscompletion.fish
+#source /home/bjohoi/.config/fish/functions/__fish_complete_directories_homemade_tf.fish
+#source /home/bjohoi/.config/fish/functions/__fish_complete_directories_homemade_puppet.fish
+#source /home/bjohoi/.config/fish/functions/__fish_complete_directories_homemade_workpass.fish
+#source /home/bjohoi/.config/fish/functions/tfjump.fish
+#source /home/bjohoi/.config/fish/functions/puppetjump.fish
+## source completions
+#source /home/bjohoi/.config/fish/completions/tfj_complete.fish
+#source /home/bjohoi/.config/fish/completions/ppj_complete.fish
+#source /home/bjohoi/.config/fish/completions/workpasscompletion.fish
 # PATHS
-set -gx PATH /home/bjohoi/.local/bin /home/bjohoi/bin /home/bjohoi/.pulumi/bin $PATH
+set -gx PATH /opt/homebrew/bin $PATH
 # set configs: https://mattermost.redpill-linpro.com/rl/pl/m89q6769ifgnmdqp8m55jabi5c
 # not working yet alias åpnestack='mkdir -p /run/user/1000/openstack/ ;gpg -d ~/.config/openstack/clouds.yaml.gpg > /run/user/1000/openstack/clouds.yaml'
 
